9b0b8d05b8e28275f06de940fa2fec479216f98f
Joshua Turgeon <jturgeon88@gmail.com>
2025-06-06 16:07:42 -0500

    feat: implement core background job infrastructure for Sidekiq

---
+++ job_marketplace_api/app/workers/application_notification_worker.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+class ApplicationNotificationWorker
+  include Sidekiq::Worker
+
+  sidekiq_options retry: 3
+
+  def perform(job_application_id, opportunity_title, job_seeker_email, client_email)
+    # Send email notification to client about the new job application
+    JobApplicationMailer.new_application_notification(
+      job_application_id,
+      opportunity_title,
+      job_seeker_email,
+      client_email
+    ).deliver_now
+  end
+end
